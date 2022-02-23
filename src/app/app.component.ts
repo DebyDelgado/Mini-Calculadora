627ec721b47d31ff79e68c9ce8d2e67994d3666a
Delgado Asmus <d.delgado.asmus@accenture.com>
2022-02-23 11:42:22 -0300

    Primer Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'miniCalculadora';
+  title = 'Mini Calculadora';
+
+  num1:number=0;
+  num2:number=0;
+  res:number=0;
+
+  sumar():void{
+
+      this.res=this.num1+this.num2;
+
+
+  }
+  restar():void{
+
+    this.res=this.num1-this.num2;
+
+
+}
 }
